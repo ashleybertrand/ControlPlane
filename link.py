d1001063c6a5564b1c54ce344489dbd1e5d50302
ashleybertrand <ashleykaybertrand@gmail.com>
2016-11-05 18:51:02 -0600

    Step 1

--- link.py
+++ link.py
@@ -2,6 +2,8 @@
 Created on Oct 12, 2016
 
 @author: mwitt_000
+
+@modified by: Megan Weller and Ashley Bertrand
 '''
 
 import queue
@@ -15,15 +17,13 @@ class Link:
     # @param from_intf_num: number of the interface on that node
     # @param to_node: node to which data will be transfered
     # @param to_intf_num: number of the interface on that node
-    # @param mtu: link maximum transmission unit
-    def __init__(self, from_node, from_intf_num, to_node, to_intf_num, mtu):
+    def __init__(self, from_node, from_intf_num, to_node, to_intf_num):
         self.from_node = from_node
         self.from_intf_num = from_intf_num
         self.to_node = to_node
         self.to_intf_num = to_intf_num
         self.in_intf = from_node.out_intf_L[from_intf_num]
         self.out_intf = to_node.in_intf_L[to_intf_num]
-        self.mtu = mtu
         
     ## called when printing the object
     def __str__(self):
@@ -34,9 +34,6 @@ class Link:
         pkt_S = self.in_intf.get()
         if pkt_S is None:
             return #return if no packet to transfer
-        if len(pkt_S) > self.mtu:
-            print('%s: packet "%s" length greater then link mtu (%d)' % (self, pkt_S, self.mtu))
-            return #return without transmitting if packet too big
         #otherwise transmit the packet
         try:
             self.out_intf.put(pkt_S)
@@ -53,6 +50,10 @@ class LinkLayer:
         ## list of links in the network
         self.link_L = []
         self.stop = False #for thread termination
+        
+    ## called when printing the object
+    def __str__(self):
+        return 'Network'
     
     ##add a Link to the network
     def add_link(self, link):
@@ -73,4 +74,3 @@ class LinkLayer:
             if self.stop:
                 print (threading.currentThread().getName() + ': Ending')
                 return
-    
--- network.py
+++ network.py
@@ -2,6 +2,8 @@
 Created on Oct 12, 2016
 
 @author: mwitt_000
+
+@modified by: Megan Weller and Ashley Bertrand
 '''
 import queue
 import threading
@@ -10,8 +12,10 @@ import threading
 ## wrapper class for a queue of packets
 class Interface:
     ## @param maxsize - the maximum size of the queue storing packets
-    def __init__(self, maxsize=0):
+    #  @param cost - of the interface used in routing
+    def __init__(self, cost=0, maxsize=0):
         self.queue = queue.Queue(maxsize);
+        self.cost = cost
     
     ##get packet from the queue interface
     def get(self):
@@ -33,12 +37,15 @@ class Interface:
 class NetworkPacket:
     ## packet encoding lengths 
     dst_addr_S_length = 5
+    prot_S_length = 1
     
     ##@param dst_addr: address of the destination host
     # @param data_S: packet payload
-    def __init__(self, dst_addr, data_S):
+    # @param prot_S: upper layer protocol for the packet (data, or control)
+    def __init__(self, dst_addr, prot_S, data_S):
         self.dst_addr = dst_addr
         self.data_S = data_S
+        self.prot_S = prot_S
         
     ## called when printing the object
     def __str__(self):
@@ -47,6 +54,12 @@ class NetworkPacket:
     ## convert packet to a byte string for transmission over links
     def to_byte_S(self):
         byte_S = str(self.dst_addr).zfill(self.dst_addr_S_length)
+        if self.prot_S == 'data':
+            byte_S += '1'
+        elif self.prot_S == 'control':
+            byte_S += '2'
+        else:
+            raise('%s: unknown prot_S option: %s' %(self, self.prot_S))
         byte_S += self.data_S
         return byte_S
     
@@ -55,8 +68,15 @@ class NetworkPacket:
     @classmethod
     def from_byte_S(self, byte_S):
         dst_addr = int(byte_S[0 : NetworkPacket.dst_addr_S_length])
-        data_S = byte_S[NetworkPacket.dst_addr_S_length : ]
-        return self(dst_addr, data_S)
+        prot_S = byte_S[NetworkPacket.dst_addr_S_length : NetworkPacket.dst_addr_S_length + NetworkPacket.prot_S_length]
+        if prot_S == '1':
+            prot_S = 'data'
+        elif prot_S == '2':
+            prot_S = 'control'
+        else:
+            raise('%s: unknown prot_S field: %s' %(self, prot_S))
+        data_S = byte_S[NetworkPacket.dst_addr_S_length + NetworkPacket.prot_S_length : ]        
+        return self(dst_addr, prot_S, data_S)
     
 
     
@@ -79,7 +99,7 @@ class Host:
     # @param dst_addr: destination address for the packet
     # @param data_S: data being transmitted to the network layer
     def udt_send(self, dst_addr, data_S):
-        p = NetworkPacket(dst_addr, data_S)
+        p = NetworkPacket(dst_addr, 'data', data_S)
         self.out_intf_L[0].put(p.to_byte_S()) #send packets always enqueued successfully
         print('%s: sending packet "%s"' % (self, p))
         
@@ -108,43 +128,113 @@ class Router:
     ##@param name: friendly router name for debugging
     # @param intf_count: the number of input and output interfaces 
     # @param max_queue_size: max queue length (passed to Interface)
-    def __init__(self, name, intf_count, max_queue_size):
+    def __init__(self, name, in_intf_count, out_intf_cost_L, rt_tbl_D, max_queue_size):
         self.stop = False #for thread termination
         self.name = name
         #create a list of interfaces
-        self.in_intf_L = [Interface(max_queue_size) for _ in range(intf_count)]
-        self.out_intf_L = [Interface(max_queue_size) for _ in range(intf_count)]
+        self.in_intf_L = [Interface(max_queue_size) for _ in range(in_intf_count)]
+        #note the number of outgoing interfaces is set up by out_intf_cost_L
+        self.out_intf_L = []
+        for cost in out_intf_cost_L:
+            self.out_intf_L.append(Interface(cost, max_queue_size))
+        #set up the routing table for connected hosts
+        self.rt_tbl_D = rt_tbl_D 
 
     ## called when printing the object
     def __str__(self):
         return 'Router_%s' % (self.name)
 
-    ## look through the content of incoming interfaces and forward to
-    # appropriate outgoing interfaces
-    def forward(self):
+    ## look through the content of incoming interfaces and 
+    # process data and control packets
+    def process_queues(self):
         for i in range(len(self.in_intf_L)):
             pkt_S = None
-            try:
-                #get packet from interface i
-                pkt_S = self.in_intf_L[i].get()
-                #if packet exists make a forwarding decision
-                if pkt_S is not None:
-                    p = NetworkPacket.from_byte_S(pkt_S) #parse a packet out
-                    # HERE you will need to implement a lookup into the 
-                    # forwarding table to find the appropriate outgoing interface
-                    # for now we assume the outgoing interface is also i
-                    self.out_intf_L[i].put(p.to_byte_S(), True)
-                    print('%s: forwarding packet "%s" from interface %d to %d' % (self, p, i, i))
-            except queue.Full:
-                print('%s: packet "%s" lost on interface %d' % (self, p, i))
-                pass
+            #get packet from interface i
+            pkt_S = self.in_intf_L[i].get()
+            #if packet exists make a forwarding decision
+            if pkt_S is not None:
+                p = NetworkPacket.from_byte_S(pkt_S) #parse a packet out
+                if p.prot_S == 'data':
+                    self.forward_packet(p,i)
+                elif p.prot_S == 'control':
+                    self.update_routes(p)
+                else:
+                    raise Exception('%s: Unknown packet type in packet %s' % (self, p))
+            
+    ## forward the packet according to the routing table
+    #  @param p Packet to forward
+    #  @param i Incoming interface number for packet p
+    def forward_packet(self, p, i):
+        try:
+            # TODO: Here you will need to implement a lookup into the 
+            # forwarding table to find the appropriate outgoing interface
+            # for now we assume the outgoing interface is also i
+            self.out_intf_L[i].put(p.to_byte_S(), True)
+            print('%s: forwarding packet "%s" from interface %d to %d' % (self, p, i, i))
+        except queue.Full:
+            print('%s: packet "%s" lost on interface %d' % (self, p, i))
+            pass
+        
+    ## forward the packet according to the routing table
+    #  @param p Packet containing routing information
+    def update_routes(self, p):
+        #TODO: add logic to update the routing tables and
+        # possibly send out routing updates
+        print('%s: Received routing update %s' % (self, p))
+        
+    ## send out route update
+    # @param i Interface number on which to send out a routing update
+    def send_routes(self, i):
+        # a sample route update packet
+        p = NetworkPacket(0, 'control', 'Sample routing table packet')
+        try:
+            #TODO: add logic to send out a route update
+            self.out_intf_L[i].put(p.to_byte_S(), True)
+            print('%s: sending routing update "%s" from interface %d' % (self, p, i))
+        except queue.Full:
+            print('%s: packet "%s" lost on interface %d' % (self, p, i))
+            pass
+        
+    ## Print routing table
+    def print_routes(self):
+        print('%s: routing table' % self)
+        #TODO: print the routes as a two dimensional table for easy inspection
+        # Currently the function just prints the route table as a dictionary
+        #print(self.rt_tbl_D)
+        
+        for i in self.rt_tbl_D:
+            host = i
+            intf_cost = self.rt_tbl_D[i]
+
+        for i in intf_cost:
+            intf = i
+            cost = intf_cost[i]
+
+        print("\n")
+        print("     Cost to")
+        print("         1 2")
+        print("------------")
+        if (host == 1 and intf == 0):
+            print("From 0 |", cost)
+            print("     1 |")
+        elif(host == 2 and intf == 0):
+            print("From 0 |  ", cost)
+            print("     1 |")
+        elif(host == 1 and intf == 1):
+            print("From 0 |")
+            print("     1 |", cost)
+        elif(host == 2 and intf == 1):
+            print("From 0 |")
+            print("     1 |  ", cost)
+        print("\n")
+
+        
                 
     ## thread target for the host to keep forwarding data
     def run(self):
         print (threading.currentThread().getName() + ': Starting')
         while True:
-            self.forward()
+            self.process_queues()
             if self.stop:
                 print (threading.currentThread().getName() + ': Ending')
-                return
-           
+                return 
